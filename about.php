68a2d6e1cedc36bb685a698c40873cb07424d73b
psyrs11@nottingham.ac.uk <psyrs11@nottingham.ac.uk>
2016-04-29 17:47:27 +0100

    You can now filter tracks by CD (only by clicking album names at the minute).
    Now adding search bars as well as actually making the about page

--- about.php
+++ about.php
@@ -1,7 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>The Compendium</title>
+</head>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Rahul Soni
- * Date: 29/04/2016
- * Time: 17:29
- */
+include('background.php');
+?>
+
+
+<h1>System for DBI coursework 2 by Rahul Soni - student number 4255222.</h1>
+<br><br>
+
+<h2>All files as well as history are available at <a href='https://github.com/TheRedshift/dbicw' target="_blank">
+        my github page.</a></h2>
+
+
+
+</body>
+</html>
